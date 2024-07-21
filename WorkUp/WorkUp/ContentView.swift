ba1630b1633dc3ea4da5e99002648e489b201304
daminoworld <ekadls757@gmail.com>
2024-07-21 20:49:41 +0900

    Add: Add WorkUp project

---
+++ WorkUp/WorkUp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WorkUp
+//
+//  Created by Damin on 7/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WorkUp/WorkUp/WorkUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkUpApp.swift
+//  WorkUp
+//
+//  Created by Damin on 7/21/24.
+//
+
+import SwiftUI
+
+@main
+struct WorkUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
